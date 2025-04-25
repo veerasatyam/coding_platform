60b251100ea4e5f430c64019536f3d69e00c121a
veerasatyam <satyamsvs788@gmail.com>
2025-04-25 15:06:23 +0530

    commit

---
+++ Leetcode/LC383.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            letters[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (letters[c - 'a'] == 0) {
+                return false;
+            }
+            letters[c - 'a']--;
+        }
+
+        return true;
+    }
+}
