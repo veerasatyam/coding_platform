d0e20285fb06fa44612e866a477693139c02f39f
veerasatyam <satyamsvs788@gmail.com>
2025-07-28 19:27:21 +0530

    commit

--- Leetcode/LC695.java
+++ Leetcode/LC695.java
@@ -1,15 +1,21 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int max=0;
-        int n=grid.length,m=<grid[0].length;
-        for(int i=0;i<n;i++) grid[i][0]+=grid[i-1][0];
-        for(int i=0;i<m;i++) grid[0][i]+=grid[0][i-1];
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;i++){
-                grid[i][j] += grid[i-1][j]+grid[i][j-1];
-                max = Math.max(max,grid[i][j]);
+       int maxA = 0;
+       int n = grid.length,m=grid[0].length;
+       for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+            if(grid[i][j]==1){
+                int area = dfs(i,j,grid);
+                maxA = Math.max(area,maxA); 
             }
-        }
-        return max;
+          }
+       }
+       return maxA;
+    }
+    private int dfs(int i,int j,int[][] grid){
+        if(i>=grid.length || j>= grid[0].length|| i<0 || j<0 || grid[i][j]!=1)  return 0;
+
+        grid[i][j]=0;
+        return 1+dfs(i-1,j,grid) + dfs(i+1,j,grid) + dfs(i,j-1,grid) + dfs(i,j+1,grid);
     }
 }
