122b64a8ba5e817d9b35b20c8d5cde7c0768624c
veerasatyam <satyamsvs788@gmail.com>
2025-08-15 09:31:30 +0530

    commit

--- Leetcode/LC342.java
+++ Leetcode/LC342.java
@@ -1,8 +1,14 @@
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         while (n % 4 == 0 && n > 0) {
+//             n = n / 4;
+//         }
+//         return n == 1;
+//     }
+// }
+
 class Solution {
     public boolean isPowerOfFour(int n) {
-        while (n % 4 == 0 && n > 0) {
-            n = n / 4;
-        }
-        return n == 1;
+       return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 }
