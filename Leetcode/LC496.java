d584cb66b2b5d0b9797cc34df5b7e705de4027a7
veerasatyam <satyamsvs788@gmail.com>
2025-08-27 11:59:05 +0530

    commit

---
+++ Leetcode/LC496.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> MDstack = new Stack<>();
+        HashMap<Integer,Integer> nextGreatest = new HashMap<>();
+        for(int num : nums2){
+            while (!MDstack.isEmpty() && MDstack.peek() < num) {
+                nextGreatest.put(MDstack.pop(), num);
+            }
+            MDstack.push(num);
+        }
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+        
+        return result;
+    }
+}
