83ad57b9b2c5cec885a7e1b27118a162cd050775
veerasatyam <satyamsvs788@gmail.com>
2025-09-04 11:35:51 +0530

    commit

--- Leetcode/LC131.java
+++ Leetcode/LC131.java
@@ -1,14 +1,40 @@
+import java.util.*;
 
-sdfytuihgfxdgfhjbknjh
 class Solution {
     List<List<String>> result;
+
     public List<List<String>> partition(String s) {
         result = new ArrayList<>();
         List<String> current = new ArrayList<>();
-        helper(s,result,current,0,"");
+        helper(s, current, 0);
         return result;
     }
-    void helper(String s,List<List<String>> result,List<String> current,int index){
-        if(index==s.length || )
+
+    void helper(String s, List<String> current, int index) {
+        if (index == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = index; i < s.length(); i++) {
+            String substring = s.substring(index, i + 1);
+            if (isPalindrome(substring)) {
+                current.add(substring);     
+                helper(s, current, i + 1);  
+                current.remove(current.size() - 1); 
+            }
+        }
+    }
+
+    boolean isPalindrome(String x) {
+        int i = 0, j = x.length() - 1;
+        while (i < j) {
+            if (x.charAt(i) != x.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
