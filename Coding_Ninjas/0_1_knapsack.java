a2f694a1a6482898515e961f1d6e3f42c8f54959
veerasatyam <satyamsvs788@gmail.com>
2025-10-20 20:13:55 +0530

    commit

--- Coding_Ninjas/0_1_knapsack.java
+++ Coding_Ninjas/0_1_knapsack.java
@@ -1,7 +1,86 @@
 import java.util.ArrayList;
+//pure recursion
+public class Solution {
+    public static int maxProfit(ArrayList<Integer> values, ArrayList<Integer> weights, int n, int w) {
+        return helper(values, weights, n - 1, w);
+    }
+
+    private static int helper(ArrayList<Integer> values, ArrayList<Integer> weights, int index, int w) {
+        if (index == 0) {
+            if (weights.get(0) <= w) return values.get(0);
+            return 0;
+        }
+        int notTake = helper(values, weights, index - 1, w);
+        int take = 0;
+        if (weights.get(index) <= w)
+            take = values.get(index) + helper(values, weights, index - 1, w - weights.get(index));
+        return Math.max(take, notTake);
+    }
+}
+
+//recursion + memorization
+
+import java.util.*;
 
 public class Solution {
-	public static int maxProfit(ArrayList<Integer> values, ArrayList<Integer> weights, int n, int w) {
-		
-	}
+    public static int maxProfit(ArrayList<Integer> values, ArrayList<Integer> weights, int n, int w) {
+        int[][] dp = new int[n][w + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return helper(values, weights, n - 1, w, dp);
+    }
+
+    private static int helper(ArrayList<Integer> values, ArrayList<Integer> weights, int index, int w, int[][] dp) {
+        if (index == 0) {
+            if (weights.get(0) <= w) return values.get(0);
+            return 0;
+        }
+        if (dp[index][w] != -1) return dp[index][w];
+        int notTake = helper(values, weights, index - 1, w, dp);
+        int take = 0;
+        if (weights.get(index) <= w)
+            take = values.get(index) + helper(values, weights, index - 1, w - weights.get(index), dp);
+        return dp[index][w] = Math.max(take, notTake);
+    }
+}
+
+
+//tabaulation 
+import java.util.*;
+
+public class Solution {
+    public static int maxProfit(ArrayList<Integer> values, ArrayList<Integer> weights, int n, int w) {
+        int[][] dp = new int[n][w + 1];
+        for (int cap = weights.get(0); cap <= w; cap++) dp[0][cap] = values.get(0);
+		 for (int i = 1; i < n; i++) {
+            for (int cap = 0; cap <= w; cap++) {
+                int notTake = dp[i - 1][cap];
+                int take = 0;
+                if (weights.get(i) <= cap)
+                    take = values.get(i) + dp[i - 1][cap - weights.get(i)];
+                dp[i][cap] = Math.max(take, notTake);
+            }
+        }
+        return dp[n - 1][w];
+        
+    }
+}
+
+//space optimization 
+import java.util.*;
+
+public class Solution {
+    public static int maxProfit(ArrayList<Integer> values, ArrayList<Integer> weights, int n, int w) {
+        int[] dp = new int[w + 1];
+        for (int cap = weights.get(0); cap <= w; cap++) dp[cap] = values.get(0);
+        for (int i = 1; i < n; i++) {
+            for (int cap = w; cap >= 0; cap--) {
+                int notTake = dp[cap];
+                int take = 0;
+                if (weights.get(i) <= cap)
+                    take = values.get(i) + dp[cap - weights.get(i)];
+                dp[cap] = Math.max(take, notTake);
+            }
+        }
+        return dp[w];
+    }
 }
--- Leetcode/LC322.java
+++ Leetcode/LC322.java
@@ -1,3 +1,75 @@
+//pure recursion 
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int res = helper(coins, coins.length - 1, amount);
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    private int helper(int[] coins, int index, int amount) {
+        if (amount == 0) return 0;
+        if (index == 0) {
+            if (amount % coins[0] == 0) return amount / coins[0];
+            return Integer.MAX_VALUE;
+        }
+        int notTake = helper(coins, index - 1, amount);
+        int take = Integer.MAX_VALUE;
+        if (coins[index] <= amount) {
+            take =1+ helper(coins, index, amount - coins[index]);
+        }
+        return Math.min(take, notTake);
+    }
+}
+
+//recusion + memorization
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length][amount+1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        int res = helper(coins, coins.length - 1, amount,dp);
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    private int helper(int[] coins, int index, int amount,int[][] dp) {
+        if (amount == 0) return 0;
+        if (index == 0) {
+            if (amount % coins[0] == 0) return amount / coins[0];
+            return Integer.MAX_VALUE;
+        }
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int notTake = helper(coins, index - 1, amount,dp);
+        int take = Integer.MAX_VALUE;
+        if (coins[index] <= amount) {
+            take =1+ helper(coins, index, amount - coins[index],dp);
+        }
+        dp[index][amount] = Math.min(take,notTake);
+        return dp[index][amount];
+    }
+}
+
+// tabulation
+
+import java.util.*;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        int INF = Integer.MAX_VALUE - 1; 
+        for (int a = 0; a <= amount; a++) {
+            if (a % coins[0] == 0) dp[0][a] = a / coins[0];
+            else dp[0][a] = INF;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int a = 0; a <= amount; a++) {
+                int notTake = dp[i - 1][a];
+                int take = INF;
+                if (coins[i] <= a) take = 1 + dp[i][a - coins[i]];
+                dp[i][a] = Math.min(take, notTake);
+            }
+        }
+        return dp[n - 1][amount] >= INF ? -1 : dp[n - 1][amount];
+    }
+}
+
+//space optimisation..
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
