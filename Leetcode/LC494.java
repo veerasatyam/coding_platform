f044b2ab3a663f80ab6e4b4aba0b43a9a74de6ea
veerasatyam <satyamsvs788@gmail.com>
2025-10-21 16:12:22 +0530

    commit

--- Leetcode/LC494.java
+++ Leetcode/LC494.java
@@ -1,3 +1,4 @@
+// recursion based
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         return count(nums, 0, 0, target);
@@ -10,5 +11,34 @@ class Solution {
         int add = count(nums, index + 1, sum + nums[index], target);
         int subtract = count(nums, index + 1, sum - nums[index], target);
         return add + subtract;
+    }
+}
+
+// recursion + memoiation linked to count partitions with given differnce problem
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+         return countPartitions(nums.length,target,nums)
+    }
+    public static int countPartitions(int n, int d, int[] arr) {
+        int sum = 0;
+        for (int num : arr) sum += num;
+        if ((sum - d) % 2 != 0 || sum < d) return 0;
+        int target = (sum - d) / 2;
+        int[][] dp = new int[n][target + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);         
+        return helper(arr, n - 1, target, dp);
+    }
+
+    private static int helper(int[] arr, int index, int target, int[][] dp) {
+        if (index == 0) {
+            if (target == 0 && arr[0] == 0) return 2;
+            if (target == 0 || target == arr[0]) return 1;
+            return 0;
+        }
+        if (dp[index][target] != -1) return dp[index][target];
+        int notTake = helper(arr, index - 1, target, dp);
+        int take = 0;
+        if (arr[index] <= target) take = helper(arr, index - 1, target - arr[index], dp);
+        return dp[index][target] = take + notTake;
     }
 }
--- Leetcode/LC518.java
+++ Leetcode/LC518.java
@@ -1,3 +1,78 @@
+//pure recursion
+class Solution {
+    public int change(int amount, int[] coins) {
+        return helper(coins, amount, coins.length - 1);
+    }
+
+    private int helper(int[] coins, int amount, int index) {
+        if (index == 0) {
+            if (amount % coins[0] == 0) return 1;
+            return 0;
+        }
+
+        int notTake = helper(coins, amount, index - 1);
+        int take = 0;
+        if (coins[index] <= amount) {
+            take = helper(coins, amount - coins[index], index);
+        }
+
+        return notTake + take;
+    }
+}
+
+// recursion + memoization
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length][amount];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(coins, amount, coins.length - 1,dp);
+    }
+
+    private int helper(int[] coins, int amount, int index,int[][] dp) {
+        if (index == 0) {
+            if (amount % coins[0] == 0) return 1;
+            return 0;
+        }
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int notTake = helper(coins, amount, index - 1,dp);
+        int take = 0;
+        if (coins[index] <= amount) {
+            take = helper(coins, amount - coins[index], index,dp);
+        }
+        dp[index][amount] = notTake + take;
+        return dp[index][amount];
+    }
+}
+
+// tabulation 
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) dp[0][t] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                int notTake = dp[i - 1][t];
+                int take = 0;
+                if (coins[i] <= t) take = dp[i][t - coins[i]];
+                dp[i][t] = notTake + take;
+            }
+        }
+
+        return dp[n - 1][amount];
+    }
+}
+
+
+
+//space optimization
 class Solution {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
