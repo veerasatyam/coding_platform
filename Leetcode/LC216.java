6374c2fd73fe2592c25ff7587a680db6825a7a5e
veerasatyam <satyamsvs788@gmail.com>
2025-11-06 17:40:17 +0530

    commit

---
+++ Leetcode/LC216.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(1, k, n, new ArrayList<>(), res);
+        return res;
+    }
+    
+    private void backtrack(int start, int k, int target, List<Integer> path, List<List<Integer>> res) {
+        if (path.size() == k) {
+            if (target == 0) res.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for (int i = start; i <= 9; i++) {
+            if (i > target) break;
+            path.add(i);
+            backtrack(i + 1, k, target - i, path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
