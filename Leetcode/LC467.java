4817ac1730d60f56003c8c62c84952fd5d1af9a3
veerasatyam <satyamsvs788@gmail.com>
2025-11-07 18:33:29 +0530

    commit

---
+++ Leetcode/LC467.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            count += expand(s, i, i);
+            count += expand(s, i, i + 1);
+        }
+        return count;
+    }
+
+    private int expand(String s, int left, int right) {
+        int cnt = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            cnt++;
+            left--;
+            right++;
+        }
+        return cnt;
+    }
+}
