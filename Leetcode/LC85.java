caee43de7ea415b2b18033704ea00a8ce4524b57
veerasatyam <satyamsvs788@gmail.com>
2025-12-08 17:41:37 +0530

    commit

--- Leetcode/LC85.java
+++ Leetcode/LC85.java
@@ -23,40 +23,19 @@ class Solution {
     }
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] p1 = new int[n];
-        int[] p2 = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            p1[i] = -1;
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+            
+            while (!stack.isEmpty() && heights[stack.peek()] >= h) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea,height*width);
             }
-            if (!st.isEmpty()) {
-                p1[i] = st.peek();
-            }
-            st.push(i);
+            stack.push(i);
         }
-
-        st.clear();
-
-        for (int i = n - 1; i >= 0; i--) {
-            p2[i] = n;
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-            if (!st.isEmpty()) {
-                p2[i] = st.peek();
-            }
-            st.push(i);
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int width = p2[i] - p1[i] - 1;
-            int area = width * heights[i];
-            ans = Math.max(ans, area);
-        }
-        return ans;
+        
+        return maxArea;
     }
 }
