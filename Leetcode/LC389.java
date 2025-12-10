5bf05aac7b397e3d77e0da8b694e167636304162
veerasatyam <satyamsvs788@gmail.com>
2025-12-10 15:32:11 +0530

    commit

---
+++ Leetcode/LC389.java
@@ -0,0 +1,8 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sumS = 0, sumT = 0;
+        for (char c : s.toCharArray()) sumS += c;
+        for (char c : t.toCharArray()) sumT += c;
+        return (char)(sumT - sumS);
+    }
+}
