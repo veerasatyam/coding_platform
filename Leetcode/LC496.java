d91bca2df5349ee08dfb713feff6a56da63d3124
veerasatyam <satyamsvs788@gmail.com>
2026-01-07 11:57:59 +0530

    commit

--- Leetcode/LC496.java
+++ Leetcode/LC496.java
@@ -1,18 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> MDstack = new Stack<>();
-        HashMap<Integer,Integer> nextGreatest = new HashMap<>();
-        for(int num : nums2){
-            while (!MDstack.isEmpty() && MDstack.peek() < num) {
-                nextGreatest.put(MDstack.pop(), num);
-            }
-            MDstack.push(num);
-        }
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
         int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = nextGreatest.getOrDefault(nums1[i], -1);
+        Arrays.fill(result, -1);
+        for (int i = 0;i<nums1.length; i++)indexMap.put(nums1[i], i);
+        for (int a : nums2) {
+            while (!stack.isEmpty() && stack.peek() < a) {
+                int val = stack.pop();
+                result[indexMap.get(val)] = a;
+            }
+            if(indexMap.containsKey(a))stack.push(a);
         }
-        
         return result;
+    }
+}
+
+
+//another approach 
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int []ans = new int[nums1.length];
+        for ( int i =0 ; i< nums1.length;i++){
+            ans[i]=-1;
+            for(int j =0 ;j< nums2.length ;j++){
+                if(nums1[i]==nums2[j]){
+                    for (int k = j+1 ; k<nums2.length ; k++)
+                    if (nums2[k] > nums1[i]) {
+                            ans[i] = nums2[k];
+                            break;
+                    }
+                     break ;
+                }
+                
+            }
+        }
+        return ans ;
     }
 }
--- Leetcode/LC503.java
+++ Leetcode/LC503.java
@@ -18,3 +18,37 @@ class Solution {
 }
 
 // optimal solution using monotonic stack
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> idx_stack = new Stack<>();
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans,-1);
+        for(int i=0;i<nums.length;i++){
+            while(!idx_stack.isEmpty() && nums[idx_stack.peek()] < nums[i]){
+                ans[idx_stack.pop()] = nums[i];
+            }
+            idx_stack.push(i);
+        }
+        for(int i=0;i<idx_stack.peek();i++){
+            while(!idx_stack.isEmpty() && nums[idx_stack.peek()] < nums[i]) ans[idx_stack.pop()] = nums[i];
+        }
+        return ans;
+    }
+}
+
+// another approach same as the second approach but with a single for loop
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> idx_stack = new Stack<>();
+        int[] ans = new int[nums.length];
+        for(int i=0;i<2*nums.length;i++){
+            int idx = i % nums.length;
+            while(!idx_stack.isEmpty() && nums[idx_stack.peek()] < nums[iidx]){
+                ans[idx_stack.pop()] = nums[idx];
+            }
+            idx_stack.push(idx);
+        }
+        return ans;
+    }
+}
