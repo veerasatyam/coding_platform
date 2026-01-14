35a94036ba88d5731b4df90a7174ca28dcac059c
veerasatyam <satyamsvs788@gmail.com>
2026-01-14 15:16:20 +0530

    commit

--- Leetcode/LC547.java
+++ Leetcode/LC547.java
@@ -22,3 +22,52 @@ class Solution {
         }
     }
 }
+
+
+
+// using disjoint set
+// you can use dfs or bfs for the Solution
+class DisjoinSet{
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjoinSet(int n){
+        for(int i=0;i<=n;i++){
+            size.add(1);
+            parent.add(i);
+        }
+    }
+    public int findParent(int node){
+        if(node==parent.get(node)) return node;
+        int ulp = findParent(parent.get(node));
+        parent.set(node,ulp);
+        return parent.get(node);
+    }
+    public void unionBySize(int u,int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp==ulp_v) return;
+        if(size.get(ulp_u) < size.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_v) + size.get(ulp_u));
+        }else{
+            parent.size(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }
+    }
+}
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        DisjoinSet ds = new DisjoinSet(isConnected.length);
+        int n = isConnected.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j] == 1) ds.unionBySize(i,j);
+            }
+        }
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(ds.findParent(i)==i) count++;
+        }
+        return count;
+    }
+}
