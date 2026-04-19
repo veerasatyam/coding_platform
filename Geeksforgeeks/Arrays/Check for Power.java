5671e89c2b83ed37e8b45450b7c959acca8f3d4a
veerasatyam <satyamsvs788@gmail.com>
2026-04-19 11:08:28 +0530

    commit

--- Geeksforgeeks/Arrays/Check for Power.java
+++ Geeksforgeeks/Arrays/Check for Power.java
@@ -1,29 +1,7 @@
-class GfG {
-    public static boolean isPower(int x, int y) {
-        if (x == 1)
-            return (y == 1);
-        if (y == 1)
-            return true;
-        int pow = x, i = 1;
-        while (pow < y) {
-            pow *= pow;
-            i *= 2;
-        }
-        if (pow == y)
-            return true;
-        int low = x, high = pow;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int exponent = (int)(Math.log(mid) / Math.log(x));
-            int result = (int)Math.pow(x, exponent);
+if (y == 1) return true;     // x^0 = 1
+    if (x <= 1) return false;    // handles x = 0 or 1 safely
 
-            if (result == y)
-                return true;
-            if (result < y)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        return false;
+    while (y % x == 0) {
+        y /= x;
     }
-}
+    return y == 1;
