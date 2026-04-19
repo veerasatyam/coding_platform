1515ef65b8d5ee771d0b54bc0261aac3b5673737
veerasatyam <satyamsvs788@gmail.com>
2026-04-19 11:08:47 +0530

    commit

--- Geeksforgeeks/Arrays/Check for Power.java
+++ Geeksforgeeks/Arrays/Check for Power.java
@@ -1,7 +1,10 @@
-if (y == 1) return true;     // x^0 = 1
-    if (x <= 1) return false;    // handles x = 0 or 1 safely
-
-    while (y % x == 0) {
-        y /= x;
+class Solution {
+    public boolean isPower(int x, int y) {
+        if (y == 1) return true; 
+        if (x <= 1) return false;   
+        while (y % x == 0) {
+            y /= x;
+        }
+        return y == 1;
     }
-    return y == 1;
+}
