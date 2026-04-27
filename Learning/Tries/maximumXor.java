4bda4f18ef80853dc274fa26a3ea8789b777ae7a
veerasatyam <satyamsvs788@gmail.com>
2026-04-27 21:49:20 +0530

    commit

---
+++ Learning/Tries/maximumXor.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+class Node{
+    Node links[];
+    Node(){
+        links = new Node[2];
+    }
+    public void put(int x,Node node){
+        links[x] = node;
+    }
+    public Node get(int x){
+        return links[x];
+    }
+    public boolean containsKey(int x){
+        return (links[x] != null);
+    }
+}
+class Trie{
+    public static Node root;
+    Trie(){
+        root = new Node();
+    }
+    public void insert(int n){
+        Node node = root;
+        for(int i = 31;i >= 0; i--){
+            int x = (n >> i) & 1;
+            if(!node.containsKey(x)){
+                node.put(x,new Node());
+                node = node.get(x);
+            }else node = node.get(x);
+        }
+    }
+    public int find_Num(int target){
+        int result = 0;
+        Node node = root;
+        for(int i=31;i>=0;i--){
+            int x = (target >> i) & 1;
+            if(node.containsKey(1-x)){
+                result = result | (1 << i);
+                node = node.get(1-x);
+            }else node = node.get(x);
+        }
+        return result;
+    }
+}
+public class maximumXor{
+	public static int maxXOR(int n, int m, ArrayList<Integer> arr1, ArrayList<Integer> arr2){
+        Trie t = new Trie();
+        for(int x : arr1) t.insert(x);
+        int max = Integer.MIN_VALUE;
+        for(int x : arr2){
+            max  = Math.max(max,t.find_Num(x));
+        }
+        return max;
+	}
+}
