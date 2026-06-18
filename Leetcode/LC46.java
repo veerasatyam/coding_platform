f5086f692faa913f238539f3c99a1b47efa48d7e
veerasatyam <satyamsvs788@gmail.com>
2026-06-18 17:02:55 +0530

    commit

--- Leetcode/LC46.java
+++ Leetcode/LC46.java
@@ -24,3 +24,33 @@ class Solution {
         }
     }
 }
+
+
+
+
+// another optimal solution
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        helper(0,nums);
+        return ans;
+    }
+    private void helper(int i,int[] nums){
+        if(i == nums.length){
+            List<Integer> curr = new ArrayList<>();
+            for(int x : nums) curr.add(x);
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int j = i; j < nums.length; j++){
+            swap(j,i,nums);
+            helper(i+1,nums);
+            swap(i,j,nums);
+        }
+    }
+    private void swap(int i,int j,int[] nums){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
--- Leetcode/LC51.java
+++ Leetcode/LC51.java
@@ -1,3 +1,4 @@
+// brute force approach 
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> solutions = new ArrayList<>();
@@ -42,5 +43,44 @@ class Solution {
         }
 
         return true;
+    }
+}
+
+
+// optimal approach 
+class Solution {
+    List<List<String>> result = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        int[] leftRow = new int[n];
+        int[] Ldiagonal = new int[2*n - 1];
+        int[] Udiagonal = new int[2*n - 1];
+        char[][] board = new char[n][n];
+        for(char[] a : board) Arrays.fill(a,'.');
+        solve(0,board,leftRow,Ldiagonal,Udiagonal);
+        return result;
+    }
+    private void solve(int row,char[][] board,int[] leftRow,int[] Ldiagonal,int[] Udiagonal){
+        if(row == board.length){
+            List<String> ans = new ArrayList<>();
+            for(char[] a : board){
+                ans.add(new String(a));
+            }
+            result.add(ans);
+            return;
+        }
+        int n = board.length;
+        for(int col = 0; col < n; col++){
+            if(leftRow[col] == 0 && Ldiagonal[n - 1 + (col - row)] == 0 && Udiagonal[row + col] == 0){
+                board[row][col] = 'Q';
+                leftRow[col] = 1;
+                Ldiagonal[n - 1 + (col - row)] = 1;
+                Udiagonal[row + col] = 1;
+                solve(row + 1,board,leftRow,Ldiagonal,Udiagonal);
+                board[row][col] = '.';
+                leftRow[col] = 0;
+                Ldiagonal[n - 1 + (col - row)] = 0;
+                Udiagonal[row + col] = 0;
+            }
+        }
     }
 }
