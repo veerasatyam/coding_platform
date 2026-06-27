4da8d3f853a6c83b4055f0dd772a8cf9da134b1f
veerasatyam <satyamsvs788@gmail.com>
2026-06-27 23:02:05 +0530

    commit

--- Leetcode/LC155.java
+++ Leetcode/LC155.java
@@ -42,3 +42,75 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+import java.util.*;
+
+// Class to implement Minimum Stack
+class MinStack {
+    private Stack<int[]> st;
+    public MinStack() {
+        st = new Stack<>();
+    }
+    public void push(int value) {
+        if (st.isEmpty()) {
+            st.push(new int[]{value, value});
+            return;
+        }
+        int mini = Math.min(getMin(), value);
+        st.push(new int[]{value, mini});
+    }
+    public void pop() {
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek()[0];
+    }
+    public int getMin() {
+        return st.peek()[1];
+    }
+}
+
+
+
+// optimal approach 
+import java.util.Stack;
+
+// Class to implement Minimum Stack
+class MinStack {
+    private Stack<Integer> st;
+    private int mini;
+    public MinStack() {
+        st = new Stack<>();
+    }
+    public void push(int value) {
+        if (st.isEmpty()) {
+            mini = value;
+            st.push(value);
+            return;
+        }
+        if (value > mini) {
+            st.push(value);
+        } else {
+            st.push(2 * value - mini);
+            mini = value;
+        }
+    }
+    public void pop() {
+        if (st.isEmpty()) return;
+        int x = st.pop();
+        if (x < mini) {
+            mini = 2 * mini - x;
+        }
+    }
+    public int top() {
+        if (st.isEmpty()) return -1;
+        int x = st.peek();
+        if (mini < x) return x;
+        return mini;
+    }
+    public int getMin() {
+        return mini;
+    }
+}
--- Leetcode/LC239.java
+++ Leetcode/LC239.java
@@ -5,11 +5,8 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> q = new LinkedList<>();
         int[] result = new int[nums.length - k + 1];
-        
-        for (int i = 0; i < k; i++) {
-            while (!q.isEmpty() && nums[i] > q.getLast()) {
-                q.pollLast();
-            }
+        for (int i = 0;i < k;i++) {
+            while (!q.isEmpty() && nums[i] > q.getLast()) q.pollLast();
             q.addLast(nums[i]);
         }
         result[0] = q.getFirst();
@@ -18,14 +15,11 @@ class Solution {
             if (nums[s - 1] == q.getFirst()) {
                 q.pollFirst();
             }
-            
             while (!q.isEmpty() && nums[e] > q.getLast()) {
                 q.pollLast();
             }
             q.addLast(nums[e]);
-            
             result[s] = q.getFirst();
-            
             s++;
             e++;
         } 
