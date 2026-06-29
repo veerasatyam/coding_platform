21ae946a45c0fd5675cef03256b625c87c088fa0
veerasatyam <satyamsvs788@gmail.com>
2026-06-29 23:04:21 +0530

    commit

--- Leetcode/LC13.java
+++ Leetcode/LC13.java
@@ -1,9 +1,6 @@
-
-import java.util.HashMap;
-
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character , Integer> hm = new HashMap<>();
+        HashMap<Character,Integer> hm = new HashMap<>();
         hm.put('I', 1);
         hm.put('V', 5);
         hm.put('X',10);
@@ -12,11 +9,9 @@ class Solution {
         hm.put('D', 500);
         hm.put('M', 1000);
         int sum =0;
-        for(int i=0;i<s.length()-1;i++)
-        {
+        for(int i=0;i < s.length();i++){
             char x = s.charAt(i);
-            if(hm.get(x)<hm.get(s.charAt(i+1)))
-            {
+            if(i != s.length() -1 && hm.get(x) < hm.get(s.charAt(i+1))){
                 int a = Math.abs(hm.get(x)-hm.get(s.charAt(i+1)));
                 sum = sum + a ;
                 i++;
@@ -25,5 +20,6 @@ class Solution {
                 sum += hm.get(x);
             }
         }
+        return sum;
     }
 }
--- Leetcode/LC5.java
+++ Leetcode/LC5.java
@@ -31,3 +31,58 @@ class Solution {
         return s.substring(start, end + 1);
     }
 }
+
+
+
+// using recursion
+class Solution {
+    public String longestPalindrome(String s) {
+        return helper(s,0,s.length() - 1);
+    }
+    private String helper(String str,int start,int end){
+        if(start > end) return "";
+        if(isPalindrome(str,start,end)){
+            return str;
+        }
+        String left = helper(str,left+1,end);
+        String right = helper(str,left,end - 1);
+        return (left.length() > right.length()) ? left : right;
+    }
+    private boolean isPalindrome(String s,int start,int end){
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
+
+// recursion + memoiation 
+class Solution {
+    public String longestPalindrome(String s) {
+        String[][] dp = new String[s.length()][s.length()];
+        return helper(s,0,s.length() - 1,dp);
+    }
+    private String helper(String str,int start,int end,String[][] dp){
+        if(start > end) return "";
+        if(dp[start][end] != null) return dp[start][end];
+        if(isPalindrome(str,start,end)) return dp[start][end] = str.substring(start,end + 1);
+        String left = helper(str,start+1,end,dp);
+        String right = helper(str,start,end - 1,dp);
+        return dp[start][end] = (left.length() > right.length()) ? left : right;
+    }
+    private boolean isPalindrome(String s,int start,int end){
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
