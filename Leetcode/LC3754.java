508f31c5a56c3c99c599ae20ee736075c1622ee9
veerasatyam <satyamsvs788@gmail.com>
2026-07-07 22:45:41 +0530

    commit

---
+++ Leetcode/LC3754.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c != '0') sb.append(c);
+        }
+        if (sb.length() == 0) return 0;
+        long x = Long.parseLong(sb.toString());
+        long sum = 0;
+        String xStr = String.valueOf(x);
+        for (char c : xStr.toCharArray()) sum += (c - '0');
+        return x * sum;
+    }
+}
+
+
+class Solution {
+    public long sumAndMultiply(int n) {
+        long num=0;
+        long sum=0;
+        long ans =0;
+        while(n!=0){
+            if(n%10!=0){
+                num = num*10+n%10;
+                sum += n%10;
+            }
+            n/=10;
+        }
+         while(num!=0){
+            ans = ans*10+num%10;
+            num/=10;
+        }
+        return ans*sum;
+
+    }
+}
