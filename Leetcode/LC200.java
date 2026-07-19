0d6f10ac44e627e7d8bc1fe783a042680b8bb65a
veerasatyam <satyamsvs788@gmail.com>
2026-07-19 19:04:06 +0530

    commit

--- Leetcode/LC200.java
+++ Leetcode/LC200.java
@@ -63,3 +63,78 @@ class Solution{
         return count;
     }
 }
+
+
+
+// using union find 
+class Solution {
+    class DisjoinSet{
+        List<Integer> parent;
+        List<Integer> size;
+        public DisjoinSet(int n){
+            parent = new ArrayList<>();
+            size = new ArrayList<>();
+            for(int i=0;i<=n;i++){
+                parent.add(i);
+                size.add(1);
+            }
+        }
+        public int findParent(int node){
+            if(node == parent.get(node)) return node;
+            int ulp = findParent(parent.get(node));
+            parent.set(node, ulp);
+            return parent.get(node);
+        }
+        public void union(int u,int v){
+            int ulp_u = findParent(u);
+            int ulp_v = findParent(v);
+            if(ulp_u == ulp_v) return;
+            if(size.get(ulp_u) < size.get(ulp_v)){
+                parent.set(ulp_u, ulp_v);
+                size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+            }else{
+                parent.set(ulp_v, ulp_u);
+                size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+            }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        DisjoinSet ds = new DisjoinSet(n*m);
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    int node = i*m + j;
+                    if(i > 0 && grid[i-1][j] == '1'){
+                        int adjNode = (i-1)*m + j;
+                        ds.union(node, adjNode);
+                    }
+                    if(j > 0 && grid[i][j-1] == '1'){
+                        int adjNode = i*m + (j-1);
+                        ds.union(node, adjNode);
+                    }
+                    if(i < n-1 && grid[i+1][j] == '1'){
+                        int adjNode = (i+1)*m + j;
+                        ds.union(node, adjNode);
+                    }
+                    if(j < m-1 && grid[i][j+1] == '1'){
+                        int adjNode = i*m + (j+1);
+                        ds.union(node, adjNode);
+                    }
+                }
+            }
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    int node = i*m + j;
+                    set.add(ds.findParent(node));
+                }
+            }
+        }
+        return set.size();
+    }
+}
