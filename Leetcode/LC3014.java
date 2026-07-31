21f2415afc1ecec6d92c3df132c9348965778b83
veerasatyam <satyamsvs788@gmail.com>
2026-07-31 16:25:54 +0530

    commit

---
+++ Leetcode/LC3014.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int pushes = 0;
+        for (int i = 0;i < n;i++) pushes += (i / 8) + 1;
+        return pushes;
+    }
+}
---
+++ Leetcode/LC3016.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray())freq[c - 'a']++;
+        Arrays.sort(freq);
+        int totalPushes = 0;
+        for (int i = 0;i < 26;i++) {
+            int count = freq[25 - i];
+            if (count == 0) break;
+            int costPerPush = (i / 8) + 1;
+            totalPushes += count * costPerPush;
+        }
+        return totalPushes;
+    }
+}
